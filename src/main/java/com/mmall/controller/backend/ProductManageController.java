8324e0446e218db80543b27f712522741c624a1c
fuxiaojie <457732323@qq.com>
2018-10-09 18:59:08 +0800

    product

---
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -0,0 +1,81 @@
+package com.mmall.controller.backend;
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Product;
+import com.mmall.pojo.User;
+import com.mmall.service.IProcuctService;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by lenovo on 2018/10/9.
+ */
+@Controller
+@RequestMapping("/manage/product")
+public class ProductManageController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @Autowired
+    private IProcuctService iProcuctService;
+
+    @RequestMapping("save.do")
+    @ResponseBody
+    public ServerResponse productSave(HttpSession session, Product product) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null)
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，请登录管理员");
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            //填充我们增加商品的业务逻辑
+            return iProcuctService.saveOrUpdateProduct(product);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    /**
+     * 更新商品的状态
+     * @param session
+     * @param productId
+     * @param status
+     * @return
+     */
+    @RequestMapping("set_sale_status.do")
+    @ResponseBody
+    public  ServerResponse setSaleStatus(HttpSession session,Integer productId, Integer status) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if(user == null)
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，请登录管理员");
+
+        if(iUserService.checkAdminRole(user).isSuccess()) {
+            //
+            return iProcuctService.setSaleStatus(productId,status);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("detail.do")
+    @ResponseBody
+    public  ServerResponse getDetail(HttpSession session,Integer productId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if(user == null)
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，请登录管理员");
+
+        if(iUserService.checkAdminRole(user).isSuccess()) {
+            // 填充业务
+            return iProcuctService.manageProductDetail(productId);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+}
---
+++ src/main/java/com/mmall/service/IProcuctService.java
@@ -0,0 +1,16 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Product;
+import com.mmall.vo.ProductDetailVo;
+
+/**
+ * Created by lenovo on 2018/10/9.
+ */
+public interface IProcuctService {
+    ServerResponse saveOrUpdateProduct(Product product);
+
+    ServerResponse<String> setSaleStatus(Integer productId, Integer status);
+
+    ServerResponse<ProductDetailVo> manageProductDetail(Integer productId);
+}
