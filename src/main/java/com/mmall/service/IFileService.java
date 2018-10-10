87081256b533af21bd6b89a078bd3766faa98f7f
fuxiaojie <457732323@qq.com>
2018-10-10 10:24:15 +0800

    upload

--- src/main/java/com/mmall/service/IFileService.java
+++ src/main/java/com/mmall/service/IFileService.java
@@ -1,7 +1,11 @@
 package com.mmall.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * Created by lenovo on 2018/10/9.
  */
 public interface IFileService {
+
+    String upload(MultipartFile file, String path);
 }
--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mmall.service.impl;
 
+import com.google.common.collect.Lists;
 import com.mmall.service.IFileService;
+import com.mmall.util.FTPUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -23,11 +25,15 @@ public class FileServiceImpl implements IFileService {
     public String upload(MultipartFile file,String path) {
         String fileName = file.getOriginalFilename();
         //获取扩展名，从后面开始获取
+        // abc.jpg   substring从后面获取第一个“.”
         String fileExtendsionName = fileName.substring(fileName.lastIndexOf(".") + 1);
+        // 防止多个人传输的文件的名字一样，因此在原来的名字前面加上UUID，保证不重复
         String uploadFileName = UUID.randomUUID().toString() + "." + fileExtendsionName;
         logger.info("开始上传文件，上传文件的文件名字：{}，上传的路径：{}，新文件名：{}",fileName,path,uploadFileName);
 
+        // 声明目录的file
         File fileDir = new File(path);
+        // 判断文件夹是否存在
         if(!fileDir.exists()) {
             fileDir.setWritable(true);
             fileDir.mkdirs();
@@ -37,12 +43,16 @@ public class FileServiceImpl implements IFileService {
             file.transferTo(targetFile);
             // 文件已经上传成功
             // todo  将targetFile上传到我们的FTP服务器上
+            FTPUtil.uploadFile(Lists.<File>newArrayList(targetFile));
 
+            // 已经长传到ftp服务器上
             // todo 上传完成之后，删除upload下面的文件
+            targetFile.delete();
         } catch (IOException e) {
             logger.error("上传文件异常",e);
             return null;
         }
+        // 目标文件的文件名字
         return targetFile.getName();
     }
 }
