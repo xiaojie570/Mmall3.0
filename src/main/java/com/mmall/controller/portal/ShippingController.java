4cea81c0b316dcad3a487a5c8099eb6b4aa87a36
fuxiaojie <457732323@qq.com>
2018-11-24 15:06:05 +0800

    添加 收货地址管理模块 注释

--- src/main/java/com/mmall/controller/portal/ShippingController.java
+++ src/main/java/com/mmall/controller/portal/ShippingController.java
@@ -24,6 +24,12 @@ public class ShippingController {
     @Autowired
     protected IShippingService iShippingService;
 
+    /**
+     * 新增收货地址
+     * @param session 用来获取用户id
+     * @param shipping 地址信息
+     * @return
+     */
     @RequestMapping("add.do")
     @ResponseBody
     public ServerResponse add(HttpSession session, Shipping shipping) {
@@ -34,6 +40,12 @@ public class ShippingController {
         return iShippingService.add(user.getId(),shipping);
     }
 
+    /**
+     * 删除收货地址
+     * @param session
+     * @param shippingId
+     * @return
+     */
     @RequestMapping("del.do")
     @ResponseBody
     public ServerResponse del(HttpSession session, Integer shippingId) {
@@ -44,7 +56,12 @@ public class ShippingController {
         return iShippingService.del(user.getId(),shippingId);
     }
 
-
+    /**
+     * 更新收货地址操作，更新就不需要返回给用户shippingId了
+     * @param session
+     * @param shipping
+     * @return
+     */
     @RequestMapping("update.do")
     @ResponseBody
     public ServerResponse update(HttpSession session, Shipping shipping) {
@@ -56,6 +73,12 @@ public class ShippingController {
     }
 
 
+    /**
+     * 查询收货地址详细信息
+     * @param session
+     * @param shippingId
+     * @return
+     */
     @RequestMapping("select.do")
     @ResponseBody
     public ServerResponse<Shipping> select(HttpSession session, Integer shippingId) {
@@ -66,6 +89,13 @@ public class ShippingController {
         return iShippingService.select(user.getId(),shippingId);
     }
 
+    /**
+     * 收货地址的分页列表
+     * @param pageNum 分页中的当前页数
+     * @param pageSize 分页中的每一页中记录的条数
+     * @param session
+     * @return
+     */
     @RequestMapping("list.do")
     @ResponseBody
     public ServerResponse<PageInfo> list(@RequestParam(value="pageNum",defaultValue = "1") int pageNum,
--- src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
+++ src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
@@ -23,11 +23,20 @@ public class ShippingServiceImpl implements IShippingService {
     @Autowired
     private ShippingMapper shippingMapper;
 
+    /**
+     * 新增收货地址
+     * 1. 使用springMVC来直接绑定对象，将userId防止在shipping中
+     * 2. 将shipping插入数据库中，然后返回生效行数，如果插入成功则将shippingId传递给前端
+     * @param userId
+     * @param shipping
+     * @return
+     */
     public ServerResponse add(Integer userId,Shipping shipping) {
         shipping.setUserId(userId);
         int rowCount = shippingMapper.insert(shipping);
         if(rowCount > 0) {
             Map result = Maps.newHashMap();
+            // 获取这个自动增长的id是通过设置mybatis层的两个关键字获得：useGeneratedKeys="true" keyProperty="id"
             result.put("shippingId",shipping.getId());
             return ServerResponse.createBySuccessMsg("新建地址成功",result);
         }
@@ -35,6 +44,12 @@ public class ShippingServiceImpl implements IShippingService {
     }
 
 
+    /**
+     * 删除收货地址。 这里防止横向越权，所以要将收货地址id与用户id都传递到数据库中
+     * @param userId  删除的用户id
+     * @param shippingId 删除的收货地址id
+     * @return
+     */
     public ServerResponse<String> del(Integer userId,Integer shippingId){
         int resultCount = shippingMapper.deleteByShippingIdUserId(userId,shippingId);
         if(resultCount > 0){
@@ -44,6 +59,13 @@ public class ShippingServiceImpl implements IShippingService {
     }
 
 
+    /**
+     * 更新收货地址信息，为了防止横向越权，需要将用户的id传递到mybatis中
+     * 注意在mybatis中更改收货地址信息sql中，userid是不能更改的，它只是作为where条件存在
+     * @param userId
+     * @param shipping
+     * @return
+     */
     public ServerResponse<String> update(Integer userId,Shipping shipping) {
         // 防止横向越权
         shipping.setUserId(userId);
@@ -54,6 +76,13 @@ public class ShippingServiceImpl implements IShippingService {
         return ServerResponse.createByErrorMessage("更新地址失败");
     }
 
+    /**
+     * 查询收货地址详细信息
+     *
+     * @param userId
+     * @param shippingId
+     * @return
+     */
     public ServerResponse<Shipping> select(Integer userId, Integer shippingId){
         Shipping shipping = shippingMapper.selectByShippingIdUserId(userId,shippingId);
         if(shipping == null){
@@ -62,6 +91,13 @@ public class ShippingServiceImpl implements IShippingService {
         return ServerResponse.createBySuccessMsg("查询地址成功",shipping);
     }
 
+    /**
+     * 收货地址的分页列表
+     * @param userId
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
     public ServerResponse<PageInfo> list(Integer userId, int pageNum,int pageSize) {
         PageHelper.startPage(pageNum,pageSize);
         List<Shipping> shippingList = shippingMapper.selectByUserId(userId);
