b04f30b357e48a8e889e224fa016d75eda5faba5
fuxiaojie <457732323@qq.com>
2018-12-28 19:18:37 +0800

    getUserInfo 重构

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -8,6 +8,7 @@ import com.mmall.service.IUserService;
 import com.mmall.util.CookieUtil;
 import com.mmall.util.JsonUtil;
 import com.mmall.util.RedisPoolUtill;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -79,11 +80,17 @@ public class UserController {
      */
     @RequestMapping(value="get_user_info.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> getUserInfo(HttpSession session) {
-        User user = (User)session.getAttribute(Const.CURRENT_USER);
+    public ServerResponse<User> getUserInfo(HttpSession session, HttpServletRequest httpServletRequest) {
+        //User user = (User)session.getAttribute(Const.CURRENT_USER);
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        if(StringUtils.isEmpty(loginToken)) {
+            return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户信息");
+        }
+        String userJson = RedisPoolUtill.get(loginToken);
+        User user = JsonUtil.String2Obj(userJson,User.class);
+
         if(user != null)
             return ServerResponse.createBySuccess(user);
-
         return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户信息");
     }
 
