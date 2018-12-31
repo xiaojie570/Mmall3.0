52a8b576baefc9d74135ceed6724437eaf03428c
fuxiaojie <457732323@qq.com>
2018-12-31 13:36:44 +0800

    springMVC 拦截器初始化

---
+++ src/main/java/com/mmall/controller/common/interceptor/AuthorityInterceptor.java
@@ -0,0 +1,34 @@
+package com.mmall.controller.common.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by lenovo on 2018/12/31.
+ */
+
+@Slf4j
+public class AuthorityInterceptor implements HandlerInterceptor{
+    // 在Controller之前
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        log.info("preHandle");
+        return false;
+    }
+
+    // 在Controller之后
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle");
+    }
+
+    // 在所有处理完之后调用的，
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        log.info("afterCompletion");
+    }
+}
