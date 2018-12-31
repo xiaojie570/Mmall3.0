c89a0d3fc7befb09f462e4a4afbcdc781c2a1143
fuxiaojie <457732323@qq.com>
2018-12-31 13:58:19 +0800

    UserManageController

--- src/main/java/com/mmall/controller/backend/UserManageController.java
+++ src/main/java/com/mmall/controller/backend/UserManageController.java
@@ -4,12 +4,17 @@ import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisShardedPoolUtill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -33,14 +38,18 @@ public class UserManageController {
      */
     @RequestMapping(value ="login.do",method = RequestMethod.GET)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
+    public ServerResponse<User> login(String username, String password, HttpSession session,
+                                      HttpServletResponse httpServletResponse) {
+
 
         ServerResponse<User> response = iUserService.login(username,password);
         if(response.isSuccess()) {
+
             User user = response.getData();
             if(user.getRole() == Const.Role.ROLE_ADMIN) {
                 // 说明登录的是管理员
-                session.setAttribute(Const.CURRENT_USER,user);
+                CookieUtil.writeLoginToken(httpServletResponse,session.getId());
+                RedisShardedPoolUtill.setEx(session.getId(),Const.RedisCacheExtime.REDIS_SESSION_EXTIME, JsonUtil.Obj2String(response.getData()));
                 return response;
             } else {
                 return ServerResponse.createByErrorMessage("不是管理员，无法登录");
