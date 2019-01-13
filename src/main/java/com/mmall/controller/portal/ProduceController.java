895ac4d7615d0f91762edfbc03b7a6d89b07e56d
fuxiaojie <457732323@qq.com>
2019-01-13 20:05:59 +0800

    ProductController list方法重构为RESTful

--- src/main/java/com/mmall/controller/portal/ProduceController.java
+++ src/main/java/com/mmall/controller/portal/ProduceController.java
@@ -8,6 +8,7 @@ import com.mmall.pojo.User;
 import com.mmall.service.IOrderService;
 import com.mmall.service.IProcuctService;
 import com.mmall.vo.ProductDetailVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -58,6 +59,24 @@ public class ProduceController {
                                          @RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
                                          @RequestParam(value = "pageSize",defaultValue = "10") int pageSize,
                                          @RequestParam(value = "orderBy",defaultValue = "") String orderBy) {
+        return iProcuctService.getProductByKeyworldCategory(keyword,categoryId,pageNum,pageSize,orderBy);
+    }
+
+
+    @RequestMapping(value = "/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value="keyword")String keyword,
+                                         @PathVariable(value = "categoryId") Integer categoryId,
+                                         @PathVariable(value = "pageNum") Integer pageNum,
+                                         @PathVariable(value = "pageSize") Integer pageSize,
+                                         @PathVariable(value = "orderBy") String orderBy) {
+        if(pageNum == null)
+            pageNum = 1;
+        if(pageSize == null)
+            pageSize = 20;
+        if(StringUtils.isBlank(orderBy)) {
+            orderBy = "price_asc";
+        }
         return iProcuctService.getProductByKeyworldCategory(keyword,categoryId,pageNum,pageSize,orderBy);
     }
 }
